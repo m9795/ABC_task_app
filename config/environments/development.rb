cf7be3f08eeaca66500c38aa94bcfc580e684269
m9795 <m.ogawa9795@gmail.com>
2022-09-06 14:37:25 +0000

    [Add]slim, bootstrap,scss

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "a33f4eeb61f14e1fa69610dd95c67015.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
