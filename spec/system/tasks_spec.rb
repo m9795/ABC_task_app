181f3c012df6636eb345a0c731cbde2219fcba05
m9795 <m.ogawa9795@gmail.com>
2022-09-20 03:20:31 +0000

    [Add]SystemSpec users,tasks

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      # ユーザーAを作成しておく
+      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      # 作成者がユーザーAであるタスクを作成しておく
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+    end
+  
+    context 'ユーザーAがログインしているとき' do
+      before do
+        # ユーザーAでログインする
+        visit login_path # ログイン画面へアクセス
+        fill_in 'メールアドレス', with: 'a@example.com' # メールアドレスを入力
+        fill_in 'パスワード', with: 'password' # パスワードを入力
+        click_button 'ログインする' # ログインボタンを押す
+      end
+      
+      it 'ユーザーAが作成したタスクが表示される' do
+        # 作成済みタスクの名称が画面上に表示されていることを確認
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+  end
+end
