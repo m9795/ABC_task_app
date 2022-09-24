648a7ee63a0343644ec77ee900608486f0bb3c11
m9795 <m.ogawa9795@gmail.com>
2022-09-24 05:00:07 +0000

    [Add]ransackのソート機能

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,4 @@
 class Task < ApplicationRecord
-  # before_validation :set_nameless_name
-
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
@@ -14,7 +12,11 @@ class Task < ApplicationRecord
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
   end
 
-  # def set_nameless_name
-  #   self.name = '名前なし' if name.blank?
-  # end
+  def self.rabsackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
